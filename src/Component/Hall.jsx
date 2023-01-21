e048be6ac9f49ce40e0837bdd9b3c92d2437f7a1
Satyam rao <100476592+satyamgitt@users.noreply.github.com>
2023-01-21 06:40:57 -0800

    hall_commit (#3)
    adb

---
+++ src/Component/Hall.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hall = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, animi minima inventore modi fugiat aspernatur officia cum magni quaerat culpa eos sit explicabo praesentium similique rerum odio recusandae illum, assumenda eveniet blanditiis unde magnam laboriosam cumque! Nesciunt veniam fugiat deleniti, adipisci officiis nihil sit libero dolore minus eligendi perferendis incidunt ratione facere architecto aperiam aut autem amet earum magnam esse aspernatur aliquid. Atque fugiat itaque dolorum. Sunt, eligendi quae nemo velit at excepturi deserunt optio asperiores beatae.</div>
+  )
+}
+
+export default Hall
