13bfd1508bd1ea4a527203b4a4cc8bdfcd951dbf
satyam rao <satyampriyam98@gmail.com>
2023-01-21 06:14:18 -0800

    jan_21_commit

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>Header
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta aut, aspernatur eaque id deleniti qui quas necessitatibus, a doloribus vel adipisci autem dicta et illum vitae asperiores placeat commodi non! Delectus autem magnam pariatur impedit quia, nobis ad tempore sint sit. Adipisci error, iure quasi autem architecto quia unde?
+
+    </div>
   )
 }
 
