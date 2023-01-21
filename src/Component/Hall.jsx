2dcc569a9b0da7027e8f57403614fc674e8cec4b
satyam rao <satyampriyam98@gmail.com>
2023-01-21 06:18:06 -0800

    jan_22_commit

---
+++ src/Component/Hall.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hall = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ducimus harum dolorem. Libero asperiores tempora, hic officiis non ab sapiente quibusdam ad optio nisi quae, rerum error aspernatur. Voluptates odio magnam ipsam a aperiam quos non eaque velit ullam libero nihil sapiente ratione magni soluta, porro dolorem sit inventore saepe?</div>
+  )
+}
+
+export default Hall
